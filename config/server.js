916c57a11b4a30db1de62105403f856a85b71a66
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2021-06-27 11:08:54 +0700

    first commit since branching

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '82e7ff21f9a269bd8d0639288333718f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a1c07375-1fdf-4aa0-838c-466c5d4e3980'
+};
